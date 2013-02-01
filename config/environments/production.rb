9cb9307c719bc511cb22c751e0b64cffaff9e006
William Boozer <BillyBoozer@Billys-MacBook-Air.local>
2013-02-01 11:17:21 -0500

    Changing the emailing settings using mailchimp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,13 @@
 Manager::Application.configure do
+   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 25, # or 587
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => "BillyBoozer",
+    :password  => "BaqTjUibJHz5E9f_qC57qg",
+    :authentication => 'login' # Mandrill supports 'plain' or 'login'
+  }
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
@@ -7,6 +16,9 @@ Manager::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
+
+  config.action_mailer.default_url_options = { :host => 'fast-brushlands-5255.herokuapp.com/' }
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
